507010d437bd8568f4539758ca02ba5f3fd82eb6
Varun23OP <varunvishwa2395@gmail.com>
2026-06-13 18:42:59 +0530

    upadate

--- custom_addons/mini_erp/models/purchase_order.py
+++ custom_addons/mini_erp/models/purchase_order.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import models, fields, api
+
+# pyrefly: ignore [missing-import]
 from odoo.exceptions import UserError
 
 
