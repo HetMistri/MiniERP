a740c56d2a4a858cce93d19349a3092ba8672501
HetMistri <ganpat.hetmistri@gmail.com>
2026-06-13 13:41:58 +0530

    Switch init-db.sh -> init-db.sql for reliable bootstrap
    - .sql files run natively by PG entrypoint (no exec perms needed)
    - Added ALTER USER odoo CREATEDB so Odoo can create its own db
    - Docker compose volume mount updated accordingly

--- init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE mini_erp OWNER odoo;
-    CREATE DATABASE odoo OWNER odoo;
-    ALTER USER odoo CREATEDB;
-EOSQL
